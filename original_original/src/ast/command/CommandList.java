d2942aacfa1a9fd207ddb1833dc812534096e792
acramin <132026903+acramin@users.noreply.github.com>
2024-11-11 23:09:10 -0300

    Update CommandList.java

--- original_original/src/ast/command/CommandList.java
+++ original_original/src/ast/command/CommandList.java
@@ -2,22 +2,30 @@ package ast.command;
 
 import ast.CodeVisitor;
 
-public class CommandList implements Command {
+public class CommandList implements Command
+{
     public CommandList commandList;
     public Command command;
 
     public CommandList(Command command, CommandList commandList) {
-        this.commandList = commandList;
+        this.commandList = commandList; 
         this.command = command;
     }
 
-    public CommandList(Command command) {
+    public CommandList (Command command) {
         this(command, null);
     }
-    
+
+    public CommandList getCommandList() {
+        return commandList;
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
     @Override
     public void accept(CodeVisitor v) {
         v.visit(this);
     }
-
 }
